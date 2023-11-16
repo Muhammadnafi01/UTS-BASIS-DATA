53d2f2999ec552ff2bcd253f9912966d3249637d
Muhammadnafi01 <151003110+Muhammadnafi01@users.noreply.github.com>
2023-11-16 23:28:20 +0700

    Add files via upload

---
+++ Jawaban2.sql
@@ -0,0 +1,14 @@
+INSERT INTO Mahasiswa (id, nama, jurusan, semester)
+VALUES (1, 'John ', 'Teknik Informatika', 3),
+       (2, 'Smith', 'Manajemen', 5),
+       (3, 'Johnson', 'Akuntansi', 2);
+			 
+			 INSERT INTO Dosen (id, nama, jabatan, departemen)
+VALUES (1, 'Dr. Ahmad', 'Dosen', 'Teknik Informatika'),
+       (2, 'Prof. kevin', 'Guru Besar', 'Manajemen'),
+       (3, 'Dr. dava', 'Dosen', 'Akuntansi');
+			 
+			 INSERT INTO MataKuliah (id, nama, sks, dosen_id)
+VALUES (1, 'Basis Data', 3, 1),
+       (2, 'Manajemen Strategik', 4, 2),
+       (3, 'Akuntansi Keuangan', 3, 3);
